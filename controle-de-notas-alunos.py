8b8c6e7420141eee1293562f334b9d07c9cb0215
fredmizuki <fredericoprado@live.com>
2025-07-04 09:32:43 -0300

    controle-de-notas-alunos.py

--- controle-de-notas-alunos.py
+++ controle-de-notas-alunos.py
@@ -1,12 +1,3 @@
-# Exercício Python 105: Faça um programa que tenha uma função notas()
-# que pode receber várias notas de alunos e vai retornar um dicionário
-# com as seguintes informações:
-# – Quantidade de notas
-# – A maior nota
-# – A menor nota
-# – A média da turma
-# – A situação (opcional)
-
 def notas(*vnotas, situacao=False):
     dic = {}
     dic['Total'] = len(vnotas)
